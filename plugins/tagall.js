bd73860e20c3a582bd1e5f2e539bc4c26364ba6b
BuDy X <85840062+MrChaby@users.noreply.github.com>
2021-08-21 12:37:45 +0530

    Welcome To WhatsAlexa 🎉💖🤗

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -3,15 +3,8 @@ let {MessageType} = require('@adiwajshing/baileys');
 let Language = require('../language');
 let Lang = Language.getString('tagall');
 
-async function checkImAdmin(message, user = message.client.user.jid) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {
-        if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
 
-WhatsAlexa.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'tag', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 
    var im = await checkImAdmin(message);
    if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
@@ -21,7 +14,7 @@ WhatsAlexa.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC},
     mesaj = '';
     grup['participants'].map(
         async (uye) => {
-            mesaj += '▫@' + uye.id.split('@')[0] + ' ';
+            mesaj += '🎯 @' + uye.id.split('@')[0] + ' ';
             jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
         }
     );
