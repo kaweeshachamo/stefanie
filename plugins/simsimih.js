e290d4fcc26a9e3b69ed15fd9ea2b5cd1a58d2bf
BuDy X <85840062+MrChaby@users.noreply.github.com>
2021-08-21 17:13:11 +0530

    Update simsimih.js

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -6,13 +6,13 @@ let td = Config.WORKTYPE == 'public' ? false : true
 let Language = require('../language');
 let Lang = Language.getString('ai');
 
-WhatsAlexa.addCommand({pattern: 'simi ?(.*)', fromMe: td, desc: Lang.SIMI_DESC}, async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'alexa ?(.*)', fromMe: td, desc: Lang.SIMI_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.INVALID_REQ);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + Lang.BOT_DIVIDER +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*Alexa  ' + Lang.BOT_DIVIDER +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUND_RESPONSE, MessageType.text);
 	}
