e1ba67f10d88ec3ec5966192ecabe8897f181e02
BuDy X <85840062+MrChaby@users.noreply.github.com>
2021-08-22 14:01:22 +0530

    😁

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -1,22 +1,16 @@
-let WhatsAlexa = require('../events');
-let {MessageType} = require('@adiwajshing/baileys');
-let fs = require('fs');
-let ffmpeg = require('fluent-ffmpeg');
-let {execFile} = require('child_process');
-let cwebp = require('cwebp-bin');
-let Config = require('../config');
-let Language = require('../language');
-let Lang = Language.getString('sticker');
+const Asena = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('sticker');
 
 if (Config.WORKTYPE == 'private') {
-
-    WhatsAlexa.addCommand({pattern: 'sticker', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
+    Asena.addCommand({pattern: 'sticker$', fromMe: true, desc: Lang.STICKER_DESC}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
@@ -29,18 +23,20 @@ if (Config.WORKTYPE == 'private') {
         });
 
         if (message.reply_message.video === false && message.reply_message.image) {
-            execFile(cwebp, [location, '-o', 'output.webp'], async err => {
-                if (err) {
-                    throw err;
-                }
-        
-                await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
             });
-            return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+
         }
 
         ffmpeg(location)
-            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('sticker.webp')
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
@@ -50,13 +46,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    WhatsAlexa.addCommand({pattern: 'sticker', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
-
-        if (message.jid === '905524317852-1612300121@g.us') {
-
-            return;
-        }
-
+    Asena.addCommand({pattern: 'sticker$', fromMe: false, desc: Lang.STICKER_DESC}, (async (message, match) => {    
 
         if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
         var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
@@ -69,18 +59,20 @@ else if (Config.WORKTYPE == 'public') {
         });
 
         if (message.reply_message.video === false && message.reply_message.image) {
-            execFile(cwebp, [location, '-o', 'output.webp'], async err => {
-                if (err) {
-                    throw err;
-                }
-        
-                await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
+            ffmpeg(location)
+                .outputOptions(["-y", "-vcodec libwebp"])
+                .videoFilters('scale=2000:2000:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=2000:2000:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
+                .save('st.webp')
+                .on('end', async () => {
+                    await message.sendMessage(fs.readFileSync('st.webp'), MessageType.sticker);
             });
-            return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+
         }
 
         ffmpeg(location)
-            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
+            .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
+            .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
             .save('sticker.webp')
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
@@ -88,4 +80,3 @@ else if (Config.WORKTYPE == 'public') {
         return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }
-
