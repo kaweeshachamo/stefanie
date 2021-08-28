37c53b06ff59d7c8f37d05ca8bf0eaf50621ffb4
Buddy X <85840062+MrChaby@users.noreply.github.com>
2021-08-28 11:57:15 +0530

    Fixed ⚙🛠

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -17,11 +17,11 @@ const heroku = new Heroku({
 let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
 
 WhatsAlexa.addCommand({pattern: 'insert ?(.*)', fromMe: true, desc: Lang.INSTALL_DESC}, (async (message, match) => {
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_URL)
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_URL, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     try {
         var url = new URL(match[1]);
     } catch {
-        return await message.sendMessage(Lang.INVALID_URL);
+        return await message.client.sendMessage(message.jid, Lang.INVALID_URL, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     }
     
     if (url.host === 'gist.github.com') {
@@ -46,11 +46,11 @@ WhatsAlexa.addCommand({pattern: 'insert ?(.*)', fromMe: true, desc: Lang.INSTALL
             require('./' + plugin_name);
         } catch (e) {
             fs.unlinkSync('/root/WhatsAlexa/plugins/' + plugin_name + '.js')
-            return await message.sendMessage(Lang.INVALID_PLUGIN + ' ```' + e + '```');
+            return await message.client.sendMessage(message.jid, Lang.INVALID_PLUGIN + ' ```' + e + '```', MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
         }
 
         await Db.installPlugin(url, plugin_name);
-        await message.client.sendMessage(message.jid, Lang.INSTALLED, MessageType.text);
+        await message.client.sendMessage(message.jid, Lang.INSTALLED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     }
 }));
 
@@ -58,36 +58,36 @@ WhatsAlexa.addCommand({pattern: 'plugin', fromMe: true, desc: Lang.PLUGIN_DESC }
     var mesaj = Lang.INSTALLED_FROM_REMOTE;
     var plugins = await Db.PluginDB.findAll();
     if (plugins.length < 1) {
-        return await message.sendMessage(Lang.NO_PLUGIN);
+        return await message.client.sendMessage(message.jid, Lang.NO_PLUGIN, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     } else {
         plugins.map(
             (plugin) => {
                 mesaj += '*' + plugin.dataValues.name + '*: ' + plugin.dataValues.url + '\n';
             }
         );
-        return await message.client.sendMessage(message.jid, mesaj, MessageType.text);
+        return await message.client.sendMessage(message.jid, mesaj, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     }
 }));
 
 WhatsAlexa.addCommand({pattern: 'remove(?: |$)(.*)', fromMe: true, desc: Lang.REMOVE_DESC}, (async (message, match) => {
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_PLUGIN);
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_PLUGIN, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     if (!match[1].startsWith('__')) match[1] = '__' + match[1];
     var plugin = await Db.PluginDB.findAll({ where: {name: match[1]} });
     if (plugin.length < 1) {
-        return await message.sendMessage(message.jid, Lang.NOT_FOUND_PLUGIN, MessageType.text);
+        return await message.sendMessage(message.jid, Lang.NOT_FOUND_PLUGIN, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
     } else {
         await plugin[0].destroy();
         delete require.cache[require.resolve('./' + match[1] + '.js')]
         fs.unlinkSync('./plugins/' + match[1] + '.js');
-        await message.client.sendMessage(message.jid, Lang.DELETED, MessageType.text);
+        await message.client.sendMessage(message.jid, Lang.DELETED, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
         
         await new Promise(r => setTimeout(r, 1000));
     
-        await message.sendMessage(NLang.AFTER_UPDATE);
+        await message.sendMessage(message.jid, NLang.AFTER_UPDATE, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
         console.log(baseURI);
         await heroku.delete(baseURI + '/dynos').catch(async (error) => {
-            await message.sendMessage(error.message);
+            await message.sendMessage(message.jid, error.message, MessageType.text, {contextInfo: { forwardingScore: 1000, isForwarded: true }, quoted: message.data })
 
         });
     }
