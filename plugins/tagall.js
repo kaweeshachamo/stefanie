a8e90c7e04ed7804c01b51ce1fa2233d018af419
@Beat_OFC_Net <85840062+MrChaby@users.noreply.github.com>
2021-09-12 15:43:08 +0530

    Update tagall 🆕

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,20 +1,11 @@
-const Alexa = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const Config = require('../config');
+let WhatsAlexa = require('../events');
+let {MessageType} = require('@adiwajshing/baileys');
+let Language = require('../language');
+let Config = require('../config');
+let TLang = Language.getString('tagall');
+let Lang = Language.getString('admin');
 
-const Language = require('../language');
-const Lang = Language.getString('tagall');
-const SLang = Language.getString('scrapers');
-
-async function checkImAdmin(message, user = message.client.user.jid) {
-    var grup = await message.client.groupMetadata(message.jid);
-    var sonuc = grup['participants'].map((member) => {
-        if (member.jid.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
-    });
-    return sonuc.includes(true);
-}
-
-Alexa.addCommand({pattern: 'tag ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, (async (message, match) => {
+WhatsAlexa.addCommand({pattern: 'tag', fromMe: true, desc: TLang.TAGALL_DESC}, (async (message, match) => {
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
 
@@ -37,7 +28,7 @@ Alexa.addCommand({pattern: 'tag ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC }, 
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '🦄 @' + uye.id.split('@')[0] + '\n';
+                    mesaj += '🥏 @' + uye.id.split('@')[0] + '\n';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
