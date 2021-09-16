0355eed55ed53e9c166cb9606d258dcf4caf052e
@Beat_OFC_Net <85840062+MrChaby@users.noreply.github.com>
2021-09-16 13:45:23 +0530

    Added Antispame system ⚙🎉

--- plugins/text_spamejs.js
+++ plugins/text_spamejs.js
@@ -12,6 +12,14 @@ let STOP_SPAM = '*Spam successfully stopped! Please wait a moment.* ✅ '
 let SPAM_DESC = 'It spam until you stop it.\n⌨️ Ex: .spam test'
 let NEED_WORD = '*Need Some Words!*'
 
+let Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+let code = ('THIS \nIS \nAN \nANTISAPM \nanti lag,\nThis \ncode \nprevents \nyour \nphone \nfrom \ngetting \nstuck \ndue \nto \nmalicious \nmessages\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'+Config.CK+'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'+Config.CK+'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'+Config.CK+'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'+Config.CK+'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'+Config.CK+'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n '+Config.CK+'\n\n')
+let FINAL = 'THIS IS AN ANTISAPM (anti lag),\nThis code prevents your phone from getting stuck due to malicious messages'
+let TMUTE = 'Trying to close temporary,\nAttempting to temporarily close the group'
+let MUT = '.mute'
+let SCXR = 'Running script....'
+
 let heroku = new Heroku({
     token: Config.HEROKU.API_KEY
 });
@@ -29,6 +37,67 @@ WhatsAlexa.addCommand({pattern: 'killspam', fromMe: true, dontAddCommandList: tr
         await message.client.sendMessage(message.jid, error.message, MessageType.text);
 
     });
+}));
+
+WhatsAlexa.addCommand({pattern: 'antispam', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+    await message.client.sendMessage(
+      message.jid,MUT, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+    await message.client.sendMessage(
+        message.jid,code, MessageType.text);
+
+        await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                               await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+
 }));
 
 WhatsAlexa.addCommand({pattern: 'spam ?(.*)', fromMe: true, dontAddCommandList: true, desc: SPAM_DESC}, (async (message, match) => {
