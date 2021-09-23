38acfca3784bb82fa2513a1e15c3fb86a8b851ae
@Beat_OFC_Net <85840062+MrChaby@users.noreply.github.com>
2021-09-23 15:47:43 +0530

    Update btn.js

--- plugins/btn.js
+++ plugins/btn.js
@@ -1,6 +1,6 @@
 // Official plugin by https://github.com/MrChaby
 
-const Alexa = require('../events');
+const WhatsAlexa = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Config = require('../config')
@@ -11,8 +11,10 @@ var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
 
+let whb = Config.WORKTYPE == 'public' ? false : true
 
-Alexa.addCommand({pattern: 'btn', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+WhatsAlexa.addCommand({pattern: 'btn', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
 // send a list message!
     const rows = [
         {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
@@ -31,3 +33,21 @@ Alexa.addCommand({pattern: 'btn', fromMe: true, dontAddCommandList: true}, (asyn
        await message.client.sendMessage(message.jid, button, MessageType.listMessage)
     
     }));
+
+WhatsAlexa.addCommand({pattern: 'btm', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+// send a buttons message!
+    const buttons = [
+        {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
+        {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
+      ]
+      
+      const buttonMessage = {
+          contentText: "Hi it's button message",
+          footerText: 'Hello World',
+          buttons: buttons,
+          headerType: 1
+      }
+      
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)
+
+}));
